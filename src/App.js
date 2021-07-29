1006a778754b5f78ce16e207cd72e8379ed15acd
Damilola Adekoya <dharmykoya38@gmail.com>
2021-07-29 02:34:51 +0100

    chore(redux):setup redux for the application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+  return <div className="App">hello</div>;
 }
 
 export default App;
