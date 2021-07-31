f3f157a717e59e3be29e0141476d241af82e1880
Damilola Adekoya <dharmykoya38@gmail.com>
2021-07-31 15:48:10 +0100

    fix the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,11 @@
 /* eslint-disable no-undef */
-import { setupServer } from "msw/node";
 import { render, screen } from "./utils/test/test-utils";
 import App from "./App";
 
-const server = setupServer(...handlers);
-
-// Enable API mocking before tests.
-beforeAll(() => server.listen());
-
-// Reset any runtime request handlers we may add during the tests.
-afterEach(() => server.resetHandlers());
-
-// Disable API mocking after the tests are done.
-afterAll(() => server.close());
-
 test("renders learn react link", () => {
-  const { debug } = render(
+  render(
     <App />,
   );
-  // console.log(45, debug);
-  debug();
   const linkElement = screen.getByText(/Jobs/i);
   expect(linkElement).toBeInTheDocument();
 });
